b5815e685252810dd1eba4d6cd9da32fd0f83b5d
haja171106 <hei.haja.2@gmail.com>
2026-07-07 14:08:56 +0300

    refactor: rename createDailyExecution overload to createDailyExecutionWithRedirectAttributes

--- src/main/java/school/hei/asa/endpoint/rest/controller/DailyExecutionController.java
+++ src/main/java/school/hei/asa/endpoint/rest/controller/DailyExecutionController.java
@@ -34,20 +34,21 @@ public class DailyExecutionController {
   }
 
   public String createDailyExecution(Authentication authentication, ThDailyExecutionForm dmeForm) {
-    return createDailyExecution(authentication, dmeForm, new RedirectAttributesModelMap());
+    return createDailyExecutionWithRedirectAttributes(
+            authentication, dmeForm, new RedirectAttributesModelMap());
   }
 
   @PostMapping("/daily-execution")
-  public String createDailyExecution(
-      Authentication authentication,
-      ThDailyExecutionForm dmeForm,
-      RedirectAttributes redirectAttributes) {
+  public String createDailyExecutionWithRedirectAttributes(
+          Authentication authentication,
+          ThDailyExecutionForm dmeForm,
+          RedirectAttributes redirectAttributes) {
     var worker = workerFromAuthentication.apply(authentication).get();
     var remainingDays = contractService.getRemainingDaysByWorker(worker);
     if (remainingDays <= 0) {
       throw new IllegalStateException(
-          "You have no more days available under your contract. Please contact your"
-              + " administrator.");
+              "You have no more days available under your contract. Please contact your"
+                      + " administrator.");
     }
 
     var dailyExecution = thDailyExecutionFormMapper.toDomain(dmeForm, worker);
@@ -58,17 +59,18 @@ public class DailyExecutionController {
     var activeContractOpt = contracts.stream().filter(c -> c.duration() != null).findFirst();
 
     if (activeContractOpt.isPresent()
-        && remainingDaysAfter < lowRemainingDaysAlertService.getLowRemainingDaysThreshold()) {
+            && remainingDaysAfter < lowRemainingDaysAlertService.getLowRemainingDaysThreshold()) {
       lowRemainingDaysAlertService.checkAndAlert(
-          worker, activeContractOpt.get(), (long) remainingDaysAfter);
+              worker, activeContractOpt.get(), (long) remainingDaysAfter);
       redirectAttributes.addFlashAttribute(
-          "toastMessage",
-          "Please note : You have "
-              + (long) remainingDaysAfter
-              + " day(s) left on your contract !");
+              "toastMessage",
+              "Please note : You have "
+                      + (long) remainingDaysAfter
+                      + " day(s) left on your contract !");
       redirectAttributes.addFlashAttribute("toastType", "warning");
     }
 
     return "redirect:/work-and-care-calendar";
   }
 }
+
