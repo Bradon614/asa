aaa1af8954c86aaa0dce6ac93c086cc9b0b48793
Yoan <yoandiny14@gmail.com>
2026-07-28 20:10:10 +0300

    test: keep alert fixtures out of WorkerServiceIT year 2026
    Use 2027 entrance dates so low-remaining alert contracts are not returned by findByYearBetween(2026, 2027).

--- src/test/resources/db/migration/V101_4__Insert_low_remaining_days_alert_fixtures.sql
+++ src/test/resources/db/migration/V101_4__Insert_low_remaining_days_alert_fixtures.sql
@@ -5,5 +5,5 @@ values ('alert-worker-below', 'Alert Worker Below'),
 
 INSERT INTO contract
     (id, worker_code, level, entrance_instant, job_title, duration_in_days, contract_bucket_key)
-VALUES ('alert-contract-below', 'alert-worker-below', 'L4P-2026', '2026-07-01 00:00:00.000000', 'Test Job', 5, 'test-key'),
-       ('alert-contract-above', 'alert-worker-above', 'L4P-2026', '2026-08-01 00:00:00.000000', 'Test Job', 15, 'test-key');
+VALUES ('alert-contract-below', 'alert-worker-below', 'L4P-2026', '2027-07-01 00:00:00.000000', 'Test Job', 5, 'test-key'),
+       ('alert-contract-above', 'alert-worker-above', 'L4P-2026', '2027-08-01 00:00:00.000000', 'Test Job', 15, 'test-key');
